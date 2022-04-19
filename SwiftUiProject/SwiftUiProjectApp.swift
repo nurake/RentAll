e7f5545dc7b492184c966832070f697e88d0a3dc
nurake <nurake98@gmail.com>
2022-04-19 10:26:05 +0600

    Initial Commit

---
+++ SwiftUiProject/SwiftUiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiProjectApp.swift
+//  SwiftUiProject
+//
+//  Created by nurake on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
